0e3267e0fa516bb0cea3eba381c0d64fd2fed4b2
Abhishek Bharadwaz <55627658+4Marsha1@users.noreply.github.com>
2021-11-03 03:38:36 +0530

    Ab test (#8)
    * added index
    
    * span added
    
    * minor add
    
    * minor add

---
+++ src/components/aboutUs/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from "./AboutUs.module.css"
+
+const AboutUs = () => {
+    return (
+        <div className={styles["About-Us-Container"]}>
+            <span>Hello World!</span><span></span><span></span><span></span>
+        </div>
+    )
+}
+
+export default AboutUs
